755e7a225af7e84f39b0781ce08c1189b9952227
carmengabrie7 <carmenelisa.gabrie7@gmail.com>
2025-12-05 08:47:22 -0600

    primera clase terminada (faltaba guardar)

--- src/lab7_hash/Entry.java
+++ src/lab7_hash/Entry.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab7_hash;
 
-/**
- *
- * @author DELL
- */
+
 public class Entry {
+    protected String username;
+    protected long pos;
+    protected Entry next;
     
+    public Entry (String username, long pos, Entry next){
+        this.username = username;
+        this.pos = pos;
+        this.next=null;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    
+    public Long getPos(){
+        return pos;
+    }
+    public void setPos(Long pos){
+        this.pos=pos;
+    }
+    
+    public Entry getNext(){
+        return next;
+    }
+    public void setNext (Entry next){
+        this.next=next;
+    }
 }
