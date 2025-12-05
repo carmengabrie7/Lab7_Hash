371dc416011d9cd71b4cf6bbe1f8f9a53c9e9368
Emartorell07 <eduardomartorell38@gmail.com>
2025-12-05 08:57:45 -0600

    commit

--- src/lab7_hash/EnumTrophy.java
+++ src/lab7_hash/EnumTrophy.java
@@ -8,6 +8,16 @@ package lab7_hash;
  *
  * @author eduar
  */
-public class EnumTrophy {
+public enum EnumTrophy {
+    PLATINO(5),ORO(3),PLATA(2),BRONCE(1);
     
+    public int puntos;
+    
+    private EnumTrophy(int points){
+        this.puntos = puntos;
+    }
+    
+    public int getPuntos(){
+        return puntos;
+    }
 }
